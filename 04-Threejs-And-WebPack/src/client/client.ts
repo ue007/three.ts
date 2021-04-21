5f27978ea457b99d845b38f8c7f3dfb4f0380058
ue007 <ue007@webgpu.info>
2021-04-21 21:28:36 +0800

    feat: add 04-Threejs-And-Webpack

---
+++ 04-Threejs-And-WebPack/src/client/client.ts
@@ -0,0 +1,58 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import {GUI} from 'three/examples/jsm/libs/dat.gui.module'
+
+const scene: THREE.Scene = new THREE.Scene()
+
+const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
+const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+
+const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+camera.position.z = 2
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+const cubeFolder = gui.addFolder("Cube")
+cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2, 0.01)
+cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2, 0.01)
+cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2, 0.01)
+cubeFolder.open()
+const cameraFolder = gui.addFolder("Camera")
+cameraFolder.add(camera.position, "z", 0, 10, 0.01)
+cameraFolder.open()
+
+var animate = function () {
+    requestAnimationFrame(animate)
+
+    controls.update()
+
+    render()
+
+    stats.update()
+};
+
+function render() {
+    renderer.render(scene, camera)
+}
+animate();
